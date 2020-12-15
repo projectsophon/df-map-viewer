1e9f01d38c57d4658b7a421724f0af13cb4675e2
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-12-15 16:59:03 -0700

    add constants file to the app

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const firstReplayBlock = 12314145;
+export const lastReplayBlock = 12491275;
+export const endTimeSeconds = 1609372800;
+// Brian's home coords
+export const defaultCenterCoords = { x: 3000, y: 3983 };
+export const defaultWidthInWorldUnits = 250;
+export const isReplay = true;
